f32a8976eae10bdd85d0614acaec5feea54129e4
Daniel Bustos Ruiz <daniel.bustos935@hotmail.com>
2023-10-12 19:39:02 -0600

    El programa crea la red neuronal y hace el entrenamiento

--- mpl.py
+++ mpl.py
@@ -2,3 +2,32 @@ import torch
 import torch.nn as nn
 import numpy as np
 import pandas as pd
+
+#!Usaremos pytorch para crear la red neuronal, se crea clase que toma como padre a "Module"
+class NeuralNet(nn.Module):
+
+    #*A traves del constructor se definen las entradas, salidas, neuronas y capas
+    def __init__(self, noInputs, noCapas, noNeuronas, noOutput):
+        #*Usa el constructor de la clase padre
+        super(NeuralNet, self).__init__()
+
+        #*Crea la primer capa de entrada y de salida
+        self.capaInput = nn.Linear(noInputs, noNeuronas)
+        self.capaOutput = nn.Linear(noNeuronas, noOutput)
+
+        #*Crea las capas ocultas y las almacena en una lista de torch
+        #*Cada capa del tipo lineal
+        self.capasOcultas = nn.ModuleList()
+        for i in range(noCapas):
+            self.capasOcultas.append(nn.Linear(noNeuronas, noNeuronas))
+
+    #*Metodo para hacer el feedforward
+    def forward(self, inputs):
+        #*Comienza por recorrer todas las capas ocultas y usar la funcion de activacion con el fin de obtener una prediccion
+        #*Inicia con la capa de entrada y recorre la lista creada de torch, se va a usar la funcion sigmoide
+        prediction = torch.sigmoid(self.capaInput(inputs))
+        for capa in self.capasOcultas:
+            prediction = torch.sigmoid(capa(prediction))
+        #*Obtiene la prediccion final (capa de salida)
+        prediction = torch.sigmoid(self.capaOutput(prediction))
+        return prediction
--- practica_3.py
+++ practica_3.py
@@ -2,3 +2,55 @@ import torch
 import torch.nn as nn
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
+
+from mpl import NeuralNet
+
+#*Lee el archivo csv y lo divide en entradas y salidas
+def readCsv(file):
+
+    data = pd.read_csv(file)
+    inputs = data.iloc[:,:2]
+    outputs = data.iloc[:,2:]
+    #*Lo divide en 80% para entrenamiento, 20% para testing
+    inTrain, inTest, outTrain, outTest = train_test_split(inputs, outputs, test_size=0.2, random_state=2)
+
+    return inputs, outputs, inTrain, inTest, outTrain, outTest
+
+#*Creamos las variables y tensores para poder crear la red neuronal
+inputs, outputs, inTrain, inTest, outTrain, outTest = readCsv("concentlite.csv")
+
+tenInTrain = torch.from_numpy(inTrain.values).float().to("cpu")
+tenInTest = torch.from_numpy(inTest.values).float().to("cpu")
+tenOutTrain = torch.from_numpy(outTrain.values).float().to("cpu")
+tenOutTest = torch.from_numpy(outTest.values).float().to("cpu")
+
+#*Creamos dos funciones que van a convertir nuestras salidas y predicciones 
+#* -1 = 0 y 1 = 1
+def transformOutput(out):
+    return torch.where(out == -1, torch.tensor(0.0), torch.tensor(1.0))
+
+def transformPrediction(pred):
+    return torch.where(pred > 0.5, torch.tensor(1.0), torch.tensor(-1.0))
+
+#*Variables para entrenar la red neuronal
+#* learning rate, funcion de perdida, epocas y optimizador para el gradiente descendiente
+learningRate = 0.001
+epochs = 2000
+lossFun = nn.BCELoss()
+
+myNet = NeuralNet(2, 3, 4, 1)
+optim = torch.optim.Adam(params=myNet.parameters(), lr=learningRate)
+
+#!Comienza a hacer el entrenamiento de la red neuronal
+print(f"Comienza el entrenamiento con {epochs} epocas")
+for i in range(epochs):
+    #*Obtiene la prediccion
+    prediction = myNet(tenInTrain)
+    #*Se calcula el error
+    perdida = lossFun(prediction, transformOutput(tenOutTrain))
+    #*Hace el backpropagation y reajusta los pesos
+    perdida.backward()
+    optim.step()
+    optim.zero_grad()
+
